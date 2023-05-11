08b805341537cc0e8cb344b620285a7a24c6a8a5
gmolak <91201575+Grzegorz-Molak@users.noreply.github.com>
2023-05-11 13:03:59 +0200

    Dodano konstruktor dla klasy User

--- src/main/java/com/bemsi/model/User.java
+++ src/main/java/com/bemsi/model/User.java
@@ -12,12 +12,18 @@ import lombok.NoArgsConstructor;
 @Table (name = "userLogins")
 public class User {
     @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     @Column(name = "user_id")
     private long id;
 
     private String login;
     private String password;
     private boolean active;
+
+    public User(String login, String password, boolean active){
+        this.login = login;
+        this.password = password;
+        this.active = active;
+    }
 
 }
