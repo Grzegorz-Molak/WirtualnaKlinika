af46c6ac23622dea279c88f540686bfc8019b77d
Zuzanna <01158303@pw.edu.pl>
2023-05-29 17:16:12 +0200

    Dodano endpoint zmiany hasła

---
+++ src/main/java/com/bemsi/DTOs/requests/ChangePasswordRequest.java
@@ -0,0 +1,13 @@
+package com.bemsi.DTOs.requests;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChangePasswordRequest {
+    String oldPassword;
+    String newPassword;
+}
